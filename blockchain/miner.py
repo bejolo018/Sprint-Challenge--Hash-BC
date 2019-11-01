ed767f7bb8ad788aedf13ea0243e87d5df5bf66b
bejolo018 <bejolo018@gmail.com>
2019-11-01 12:26:45 -0500

    receiving bug, almost there I think though

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -22,10 +22,14 @@ def proof_of_work(last_proof):
     """
 
     start = timer()
+    last_hash = str(last_proof).encode()
 
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+
+    while valid_proof(last_hash, proof) is False: 
+        proof +=1 
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +44,10 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
